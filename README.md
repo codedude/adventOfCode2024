43401529e7095147ce4c9d77a18ca70d612b6d98
codedude <test@mail.com>
2024-12-02 12:47:52 +0100

    add conf

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # adventOfCode2024
 Solutions of the advent of code 2024 in Go
+
+[https://adventofcode.com/](https://adventofcode.com/)
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	ret := run(os.Args)
+	os.Exit(ret)
+}
+
+func run(args []string) int {
+	return 0
+}
